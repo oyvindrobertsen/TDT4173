1d6032b72afe734284df42e0a6167a14c9edc8b6
Peteraaser <peteraaser@gmail.com>
2016-04-19 16:18:42 +0200

    Add different granularities for oriented gradients

--- project/preprocessing.py
+++ project/preprocessing.py
@@ -4,6 +4,8 @@ import numpy as np
 from skimage.feature import hog
 from skimage import data, color, exposure
 
+from functools import reduce
+
 def binarize(images):
     images = images.reshape((len(images), 400))
     images = preprocessing.scale(images)
@@ -13,13 +15,18 @@ def binarize(images):
 
     return images
 
-def oriented_gradients(images, celldim=(5, 5), imageshape=(20,20)):
+def oriented_gradients(images, imageshape=(20,20), celldims=[4, 5, 8]):
 
     images = images.reshape((len(images), imageshape[0], imageshape[1]))
 
-    def h(image):
-        return hog(image, orientations = 4, pixels_per_cell = (celldim[0], celldim[1]),  
+    # celldims = [x/imageshape[0] for x in [2.0, 4.0, 8.0]]
+
+    def h(dim, image):
+        return hog(image, orientations = 4, pixels_per_cell = (dim, dim),  
                 cells_per_block = (1,1), visualise=False, feature_vector=True)
 
-    return np.array([h(image) for image in images])
+    def pool(image):
+        return reduce(lambda accumulator, x: np.concatenate((accumulator, h(x, image))), celldims, [])
+
+    return np.array([pool(image) for image in images])
 
