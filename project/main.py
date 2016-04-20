c99a06dc4d9f4405d4a6579a29be4e18c4776e88
Mathias Ose <mathias.ose@gmail.com>
2016-04-21 01:35:51 +0200

    Change splitting algo to support different ratios

--- project/main.py
+++ project/main.py
@@ -11,12 +11,14 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 
 from preprocessing import binarize, oriented_gradients, rpca
+from preprocessing import binarize, oriented_gradients
 from read_dataset import DATA_SHAPE, get_dataset
 from utils import int_to_letter
 
 """
 Modified from http://scikit-learn.org/stable/auto_examples/classification/plot_digits_classification.html#example-classification-plot-digits-classification-py
 """
+
 
 def load_dataset(n=25):
     # each row of the matrix is x pixel intensity values and 1 value representing the class of the image
@@ -31,28 +33,44 @@ def load_dataset(n=25):
 
     return Bunch(data=image_data,
                  target=target.astype(np.int),
-                 target_names=np.arange(ALPHABET_SIZE),
+                 target_names=np.arange(n),
                  images=images,
                  DESCR='')
 
 
-def split_dataset(dataset):
-    """
-    Split into two ~equal-sized parts
-    """
-    data = dataset.images
+def split_dataset(dataset, split=0.8):
+    training_data = []
+    test_data = []
 
-    training_data = data[0::2]
-    test_data = data[1::2]
+    training_target = []
+    test_target = []
 
-    training_target = dataset.target[0::2]
-    test_target = dataset.target[1::2]
+    training_images = []
+    test_images = []
 
-    training_images = dataset.images[0::2]
-    test_images = dataset.images[1::2]
+    c = 0
+    for data, target, image in zip(dataset.data, dataset.target, dataset.images):
+        if c / 10 < split:
+            training_data.append(data)
+            training_target.append(target)
+            training_images.append(image)
+        else:
+            test_data.append(data)
+            test_target.append(target)
+            test_images.append(image)
 
-    training = Bunch(data=training_data, target=training_target, images=training_images)
-    test = Bunch(data=test_data, target=test_target, images=test_images)
+        c = (c + 1) % 10
+
+    training = Bunch(
+        data=np.array(training_data),
+        target=np.array(training_target),
+        images=np.array(training_images)
+    )
+    test = Bunch(
+        data=np.array(test_data),
+        target=np.array(test_target),
+        images=np.array(test_images)
+    )
 
     return training, test
 
@@ -72,6 +90,7 @@ def visualize(rows):
 
 
 def train_and_test_classifier(dataset, classifier, preprocessing_func=None, visualize_n=0):
+    np.set_printoptions(threshold=np.nan, linewidth=np.nan)
     training, test = split_dataset(dataset)
 
     if preprocessing_func:
@@ -100,9 +119,8 @@ def train_and_test_classifier(dataset, classifier, preprocessing_func=None, visu
 
     return classifier
 
-if __name__ == '__main__':
-    np.set_printoptions(threshold=np.nan, linewidth=np.nan)
 
+if __name__ == '__main__':
     ALPHABET_SIZE = 25
     dataset = load_dataset(n=ALPHABET_SIZE)
 
